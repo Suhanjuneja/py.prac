96fe6198e8f33c3d11acc8b177838b847883c8ca
Suhanjuneja <junejasuhan224@gmail.com>
2024-12-27 06:42:58 +0530

    Update 9.py

--- 9.py
+++ 9.py
@@ -31,3 +31,24 @@ def reverse_words_in_file(filename):
     print(' '.join(reversed(words)))
 
 reverse_words_in_file("sample.txt")
+
+D.
+def copy_lines_to_files(input_file):
+    # Open the files for reading and writing
+    infile = open(input_file, 'r')
+    file1 = open('File1.txt', 'w')
+    file2 = open('File2.txt', 'w')
+    
+    line_number = 1 
+    for line in infile:
+        if line_number % 2 == 0:
+            file1.write(line)
+        else:
+            file2.write(line)
+        line_number += 1  
+      
+    infile.close()
+    file1.close()
+    file2.close()
+
+    print("Lines copied successfully to File1.txt and File2.txt.")
